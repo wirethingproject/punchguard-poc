fee5681c9ea16446235b0fe08d7a5cdca1857475
Fabio Mierlo <fmierlo@users.noreply.github.com>
2023-01-07 02:57:17 -0300

    Added initial code and tests for ondemandflow

---
+++ v1/ondemandflow.go
@@ -0,0 +1,17 @@
+package v1
+
+type OnDemandFlow struct {
+	BaseFlow
+}
+
+func NewOnDemand(id Id, signal Signal, otr Otr, punch Punch, guard Guard) (Flow, error) {
+	o := new(OnDemandFlow)
+	if err := o.Init(id, signal, otr, punch, guard); err != nil {
+		return nil, err
+	}
+	return o, nil
+}
+
+func (o *OnDemandFlow) Run() *Control {
+	return nil
+}
